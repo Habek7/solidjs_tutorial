6317ef46cd82251bb6a09248ab1a5088e898435c
Habek7 <markohabek06gmail.com>
2024-09-16 10:48:57 +0200

    SolidJs_Skola

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import styles from './App.module.css';
+import MyComponent from './MyComponent';
+
+function App() {
+  return (
+  <>
+    <div class={styles.App}>Dobar dan, svijete!</div>
+    <MyComponent/>
+  </>
+  );
+}
+
+export default App;
+
