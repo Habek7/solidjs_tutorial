4304a29ec2639e9017ab8942c656a0f6d81c1948
Habek7 <markohabek06gmail.com>
2024-09-23 09:19:47 +0200

    idegas

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 import styles from './App.module.css';
 import MyComponent from './MyComponent';
+import Counter from './Counter';
+import Counter2 from './Counter2';
+import Login from './Login';
 
 function App() {
   return (
   <>
     <div class={styles.App}>Dobar dan, svijete!</div>
     <MyComponent/>
+    <Counter/>
+    <Counter2/>
+    <Login/>
   </>
   );
 }
---
+++ src/Login.jsx
@@ -0,0 +1,14 @@
+import { createSignal, Show } from "solid-js"
+
+export default function Login() {
+    const [loggedin, SetLoggedin] = createSignal(false);
+    const toggle = () => SetLoggedin(!loggedin());
+
+    return (
+        <div>
+            <Show when={loggedin())
+                fallback=<button onclick={toggle}>Log in</button>
+                <button onclick={toggle}>Log out</button>
+        </div>
+    );
+}
