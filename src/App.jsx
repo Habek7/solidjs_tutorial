9f2b999cfbe9c58615d8f34943a9b7e3b3c1adb5
Habek7 <markohabek06gmail.com>
2024-10-07 09:15:34 +0200

    7.10 gotovo

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,17 @@ import Login from './Login';
 import Flow from './Flow';
 import Menu from './Menu';
 import Dynamo from './Dynamo';
+import Events from './Events';
+import Styles from './Styles';
+import Refs from './Refs';
 
 function App() {
   return (
   <>
     <div class={styles.App}>Dobar dan, svijete!</div>
-    <MyComponent/>
-    <Counter/>
-    <Counter2/>
-    <Login/>
-    <Flow/>
-    <Menu/>
-    <Dynamo/>
+    <Events/>
+    <Styles/>
+    <Refs/>
   </>
   );
 }
